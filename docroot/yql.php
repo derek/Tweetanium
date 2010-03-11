a06c3e05908283b28ad6feee1002cb0746771276
1620 <drgath@gmail.com>
2010-03-10 23:17:13 -0500

    Cleanup

--- docroot/yql.php
+++ docroot/yql.php
@@ -1,31 +1,17 @@
 <?
 	require_once('../config.php');
-	
-	header("Content-type: application/json");
-	
-	$connection = new TwitterOAuth(TWITTER_OAUTH_CONSUMER_KEY, TWITTER_OAUTH_CONSUMER_SECRET, $_SESSION['access_token']['oauth_token'], $_SESSION['access_token']['oauth_token_secret']);
 
 	$yql = $_REQUEST['yql'];
-	
-		if (!isset($_SESSION['access_token']))
-		{
-			echo json_encode(array(
-				"error"	=> "You are not logged in"
-			));
-			die();
-		}
-		else
-		{
-			$yql = str_replace("#oauth#", " oauth_consumer_key='" . TWITTER_OAUTH_CONSUMER_KEY . "' 
-			AND oauth_consumer_secret = '" . TWITTER_OAUTH_CONSUMER_SECRET . "' 
-			AND oauth_token = '" . $_SESSION['access_token']['oauth_token'] . "' 
-			AND oauth_token_secret = '" . $_SESSION['access_token']['oauth_token_secret'] . "' ", $yql);
 
-			$yql = str_replace("#oauth_consumer_key#", 		TWITTER_OAUTH_CONSUMER_KEY, $yql);
-			$yql = str_replace("#oauth_consumer_secret#", 	TWITTER_OAUTH_CONSUMER_SECRET, $yql);
-			$yql = str_replace("#oauth_token#", 			$_SESSION['access_token']['oauth_token'], $yql);
-			$yql = str_replace("#oauth_token_secret#", 		$_SESSION['access_token']['oauth_token_secret'], $yql);
-		}
+	$yql = str_replace("#oauth#", " oauth_consumer_key='" . TWITTER_OAUTH_CONSUMER_KEY . "' 
+	AND oauth_consumer_secret = '" . TWITTER_OAUTH_CONSUMER_SECRET . "' 
+	AND oauth_token = '" . $_SESSION['access_token']['oauth_token'] . "' 
+	AND oauth_token_secret = '" . $_SESSION['access_token']['oauth_token_secret'] . "' ", $yql);
+
+	$yql = str_replace("#oauth_consumer_key#", 		TWITTER_OAUTH_CONSUMER_KEY, $yql);
+	$yql = str_replace("#oauth_consumer_secret#", 	TWITTER_OAUTH_CONSUMER_SECRET, $yql);
+	$yql = str_replace("#oauth_token#", 			$_SESSION['access_token']['oauth_token'], $yql);
+	$yql = str_replace("#oauth_token_secret#", 		$_SESSION['access_token']['oauth_token_secret'], $yql);
 	
 	$yql = stripslashes($yql);
 	//die($yql);
@@ -48,5 +34,6 @@
 	
 	//unset($json->query->uri); // We never want this returned
 	
+	header("Content-type: application/json");
 	echo json_encode($json->query);
 ?>
