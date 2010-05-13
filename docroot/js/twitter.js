08b253625e662ed02621ad1483d58c98778a347c
1620 <drgath@gmail.com>
2010-05-13 04:40:56 -0400

    Couple bug fixes

--- docroot/js/twitter.js
+++ docroot/js/twitter.js
@@ -15,48 +15,53 @@ YUI.add('Twitter', function (Y) {
 	Y.Twitter = {
 		
 		call : function (request, callback, where, context) {
-			var  httpMethod, responseHandler, whereText, yql;
+			var  httpMethod, oauth, responseHandler, whereText, yql;
 			
 			httpMethod = "GET";
 			responseHandler = false;
 			yql = false;
+			oauth = false;
 			
 			if (request.type === "timeline") {
 				
 				switch (request.timeline) {
 				
-				case "dmin":	
-					yql = 'SELECT * FROM twitter.directmessages WHERE count=50 AND ' + where.field + ' = ' + where.value + '';
+				case "dmin":
+					oauth = true;
+					yql = 'SELECT * FROM twitter.directmessages WHERE count=50 AND ' + where.field + ' = ' + where.value + ' AND #oauth#';
 					break;
 
-				case "dmout":	
-					yql = 'SELECT * FROM twitter.directmessages.sent WHERE count=50 AND ' + where.field + ' = ' + where.value + '';
+				case "dmout":
+					oauth = true;	
+					yql = 'SELECT * FROM twitter.directmessages.sent WHERE count=50 AND ' + where.field + ' = ' + where.value + ' AND #oauth#';
 					break;
 				
 				case "favorites":
-					yql = 'SELECT * FROM twitter.favorites WHERE id=' + user_id + '';
+					oauth = true;
+					yql = 'SELECT * FROM twitter.favorites WHERE id=' + user_id + ' AND #oauth#';
 					break;
 
 				case "home":
-					yql = 'SELECT * FROM twitter.status.timeline.friends WHERE count=50 AND ' + where.field + ' = ' + where.value + '';
+					oauth = true;
+					yql = 'SELECT * FROM twitter.status.timeline.friends WHERE count=50 AND ' + where.field + ' = ' + where.value + ' AND #oauth#';
 					break;
 					
 				case "mentions":
+					oauth = true;
 					//yql = 'use "http://github.com/zachgraves/yql-tables/raw/master/twitter/twitter.status.mentions.xml" as twitter.status.mentions;';
-					yql = 'SELECT * FROM twitter.status.mentions WHERE count=50 AND ' + where.field + ' = ' + where.value + '';
+					yql = 'SELECT * FROM twitter.status.mentions WHERE count=50 AND ' + where.field + ' = ' + where.value + ' AND #oauth#';
 					break;
 
 				case "sent":
-					yql = 'SELECT * FROM twitter.status.timeline.user WHERE count=50 AND ' + where.field + ' = ' + where.value + '';
+					oauth = true;
+					yql = 'SELECT * FROM twitter.status.timeline.user WHERE count=50 AND ' + where.field + ' = ' + where.value + ' AND #oauth#';
 					break;
 
 				default:
 					throw ("Invalid Twitter API method");
 						
 				}
-				
-				yql = yql + '  AND oauth_token = "' + oauth_token + '" AND oauth_token_secret = "' + oauth_token_secret + '";';
-				
+								
 				responseHandler = this.tweetHandler;
 			}
 			
@@ -73,12 +78,12 @@ YUI.add('Twitter', function (Y) {
 			
 			else if (request.type === "list") {
 				//yql = 'use "http://github.com/drgath/yql-tables/raw/master/twitter/twitter.lists.statuses.xml" as twitter.lists.statuses;';
-				yql = 'SELECT * FROM twitter.lists.statuses WHERE user="' + screen_name + '" AND list_id="' + request.list + '" AND per_page=100 AND ' + where.field + ' = ' + where.value + ';';
+				yql = 'SELECT * FROM twitter.lists.statuses WHERE user="' + screen_name + '" AND list_id="' + request.list + '" AND per_page=100 AND ' + where.field + ' = ' + where.value + '';
 				responseHandler = this.tweetHandler;
 			}
 			
 			else if (request.type === "lists") {
-				yql = 'SELECT * FROM twitter.lists WHERE user="' + screen_name + '";';
+				yql = 'SELECT * FROM twitter.lists WHERE user="' + screen_name + '" AND #oauth#';
 				responseHandler = this.listHander;
 			}
 			
@@ -90,17 +95,17 @@ YUI.add('Twitter', function (Y) {
 			}
 			
 			else if (request.type === "trends") {
-				yql = 'SELECT * FROM twitter.trends.current;';
+				yql = 'SELECT * FROM twitter.trends.current';
 				responseHandler = this.trendsHandler;
 			}
 			
 			else if (request.type === "saved_searches") {
-				yql = 'SELECT * FROM twitter.search.saved;';
+				yql = 'SELECT * FROM twitter.search.saved WHERE #oauth#';
 				responseHandler = this.savedSearchHandler;
 			}
 			
 			else if (request.type === "rate_limit_status") {
-				yql = 'SELECT * FROM twitter.account.ratelimit;';
+				yql = 'SELECT * FROM twitter.account.ratelimit WHERE #oauth#';
 				responseHandler = this.rateLimitHandler;
 			}
 			
@@ -109,11 +114,11 @@ YUI.add('Twitter', function (Y) {
 			}
 			
 			if (yql) {
-				console.log("Executing: " + yql);
-				//Or the callback approach
+				yql = yql.replace("#oauth#", ' oauth_token = "' + oauth_token + '" AND oauth_token_secret = "' + oauth_token_secret + '"');
+				//console.log("Executing: " + yql);
 				new Y.yql(yql, function(r) {
 					responseHandler(r.query, callback, context);
-				}, {env: "store://tweetanium.net/tweetanium01"});
+				}, {env: "store://tweetanium.net/tweetanium04"});
 			}
 			else {
 				throw new Error("No YQL defined");
